111a2d1aca6b903c334d4a7c5a3da323602cbb50
BrianMcLucky <volker415@gmail.com>
2022-01-03 21:15:23 +0300

    dz 5

--- PycharmProjects/DjangoGeekShop/DjangoGeekShop/settings.py
+++ PycharmProjects/DjangoGeekShop/DjangoGeekShop/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'DjangoGeekShop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DjangoGeekShop',
+        'NAME': 'djangogeekshop',
         'USER': 'postgres',
     }
 }
