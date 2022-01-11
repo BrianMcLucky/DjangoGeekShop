2eaea1a4cf5c27f0f4415f8dadcc2d628d1fda55
BrianMcLucky <volker415@gmail.com>
2022-01-11 18:47:41 +0300

    update project

--- PycharmProjects/DjangoGeekShop/DjangoGeekShop/settings.py
+++ PycharmProjects/DjangoGeekShop/DjangoGeekShop/settings.py
@@ -89,21 +89,21 @@ WSGI_APPLICATION = 'DjangoGeekShop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'djangogeekshop',
-#         'USER': 'postgres',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogeekshop',
+        'USER': 'postgres',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
