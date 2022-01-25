63616bc18f84d6bde4b3bdd2423f3a0ab1211240
BrianMcLucky <volker415@gmail.com>
2022-01-25 21:01:55 +0300

    lesson7

--- PycharmProjects/DjangoGeekShop/mainapp/views.py
+++ PycharmProjects/DjangoGeekShop/mainapp/views.py
@@ -57,7 +57,6 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-# @cache_page(3600)
 @never_cache
 def products(request, id_category=None, page=1):
     context = {
